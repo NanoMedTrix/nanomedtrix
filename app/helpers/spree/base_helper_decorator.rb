0c2f1aece2d945496095db2829c877f4433aff67
Matthew Henry <matthew.gordon.henry@gmail.com>
2013-12-26 08:29:46 -0600

    bundle update, schema reset, and empty cart fix

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -2,8 +2,14 @@ module Spree
 	module BaseHelper
 		def link_to_cart( text = nil )
 			text = text ? h( text ) : Spree.t( 'cart' )	 
-			text = "<span class=\"glyphicon glyphicon-shopping-cart\"></span> #{ text } <span class=\"badge\">#{ current_order.item_count }</span>".html_safe
-				
+
+			if current_order.nil? or current_order.item_count.zero?
+				order_count = "0"
+			else
+				order_count = "#{ current_order.item_count }"
+			end
+
+			text = "<span class=\"glyphicon glyphicon-shopping-cart\"></span> #{ text } <span class=\"badge\">#{ order_count }</span>".html_safe
 			link_to text, spree.cart_path
 		end
 	end
