564fcee8fd41327a327c91933881ea25f7faa8b9
Matthew Henry <matthew.gordon.henry@gmail.com>
2014-01-02 13:48:06 -0600

    option to run db populating

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,31 +11,33 @@ if defined?( Spree::Auth )
 	puts "==  Spree::Auth seeded ========================================================"
 end
 
-puts " "
-puts "==  Populating databases ======================================================"
-%w[
-	payment_methods
-	shipping_categories
-	shipping_methods
-	tax_categories
-	tax_rates
-	products
-	taxonomies
-	taxons 
-	option_types
-	option_values
-	product_option_types
-	product_properties
-	prototypes 
-	variants
-	stock
-	assets
-	addresses
-	orders
-	payments
-].each do | filename |
-	file = File.join( File.dirname( __FILE__ ), 'fake', "#{ filename }.rb" )
-	puts "loading ruby #{ file } "
-	require file
+if ENV[ 'populate' ]
+	puts " "
+	puts "==  Populating databases ======================================================"
+	%w[
+		payment_methods
+		shipping_categories
+		shipping_methods
+		tax_categories
+		tax_rates
+		products
+		taxonomies
+		taxons 
+		option_types
+		option_values
+		product_option_types
+		product_properties
+		prototypes 
+		variants
+		stock
+		assets
+		addresses
+		orders
+		payments
+	].each do | filename |
+		file = File.join( File.dirname( __FILE__ ), 'fake', "#{ filename }.rb" )
+		puts "loading ruby #{ file } "
+		require file
+	end
+	puts "==  Populated databases ======================================================="
 end
-puts "==  Populated databases ======================================================="
