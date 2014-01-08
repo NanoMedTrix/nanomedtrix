52f8e787e8b74088498b9a21efa9235187edc836
Matthew Henry <matthew.gordon.henry@gmail.com>
2014-01-07 23:21:30 -0600

    quick tinymce setup

--- app/overrides/editor_js.rb
+++ app/overrides/editor_js.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "include_rich_text_js",
                      :insert_bottom => "[data-hook='admin_footer_scripts']",
-                     :text => "<%= tinymce_assets %>")
+                     :text => "<%= render :partial => \"spree/admin/shared/editor_engines/#{"TinyMCE".underscore}\", :locals => { :ids => ['product_description', 'page_body']} %>")
