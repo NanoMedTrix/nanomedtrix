d761cc3a8902f66256cad84d73382090c6770199
Matthew Henry <mhenry@healthcare.uiowa.edu>
2014-01-09 15:32:10 -0600

    commit for pull later

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,16 @@ Nanomedtrix::Application.routes.draw do
 
 		resources :news_articles
 
-		match '/news',                         to: 'news_articles#index',    as: :news
-		match '/news/feed',                    to: 'news_articles#feed',     as: :news_feed, format: :rss
-		match '/news/tag/:tag',                to: 'news_articles#tag',      as: :news_tag
-		match '/news/author/:author',          to: 'news_articles#author',   as: :news_author
-		match '/news/category/:category',      to: 'news_articles#category', as: :news_category
-		match '/news/:year/:month/:day/:slug', to: 'news_articles#show',     as: :news_entry_permalink
-		match '/news/:year(/:month)(/:day)',   to: 'news_articles#archive',  as: :news_archive, constraints: {
-																																															year:  /(19|20)\d{2}/, 
-																																															month: /[01]?\d/, 
-																																															day:   /[0-3]?\d/
-																																														}
+		match '/news',                         to: 'news_articles#index',    via: 'get', as: :news
+		match '/news/feed',                    to: 'news_articles#feed',     via: 'get', as: :news_feed, format: :rss
+		match '/news/tag/:tag',                to: 'news_articles#tag',      via: 'get', as: :news_tag
+		match '/news/author/:author',          to: 'news_articles#author',   via: 'get', as: :news_author
+		match '/news/category/:category',      to: 'news_articles#category', via: 'get', as: :news_category
+		match '/news/:year/:month/:day/:slug', to: 'news_articles#show',     via: 'get', as: :news_entry_permalink
+		match '/news/:year(/:month)(/:day)',   to: 'news_articles#archive',  via: 'get', as: :news_archive, constraints: {
+																																																					year:  /(19|20)\d{2}/, 
+																																																					month: /[01]?\d/, 
+																																																					day:   /[0-3]?\d/
+																																																				}
 	end
 end
