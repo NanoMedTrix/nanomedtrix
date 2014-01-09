7e39c8881e88088962a26c0823fc73b4da6fd656
Matthew Henry <matthew.gordon.henry@gmail.com>
2014-01-08 18:12:35 -0600

    minor tinymce tweaks

--- app/overrides/editor_js.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                     :name => "include_rich_text_js",
-                     :insert_bottom => "[data-hook='admin_footer_scripts']",
-                     :text => "<%= render :partial => \"spree/admin/shared/editor_engines/#{"TinyMCE".underscore}\", :locals => { :ids => ['product_description', 'page_body']} %>")
