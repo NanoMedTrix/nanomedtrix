6470a0bec2faf790e9bb9e9baddd7457d545d348
Matthew Henry <mhenry@healthcare.uiowa.edu>
2014-01-09 20:06:14 -0600

    update after pull

--- app/overrides/disable_spree_static_content_admin_tab.rb
+++ app/overrides/disable_spree_static_content_admin_tab.rb
@@ -1,10 +1,3 @@
-# undo spree_auth_devise admin tab link
-Deface::Override.new(
-	virtual_path: 'spree/admin/shared/_menu',
-	name: 'user_admin_tabs',
-	disabled: true
-)
-
 # undo spree_static_content admin tab link
 Deface::Override.new(
 	virtual_path: 'spree/admin/shared/_menu',
