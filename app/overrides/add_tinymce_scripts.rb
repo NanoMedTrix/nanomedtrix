e140f5c287782486343d9580e264dfe9a36b9b0f
Matthew Henry <mhenry@healthcare.uiowa.edu>
2014-01-14 18:15:19 -0600

    formatting

--- app/overrides/add_tinymce_scripts.rb
+++ app/overrides/add_tinymce_scripts.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(
-        virtual_path: 'spree/layouts/admin',
-        name: 'add_tinymce_scripts',
+        virtual_path:  'spree/layouts/admin',
+        name:          'add_tinymce_scripts',
         insert_bottom: "[data-hook='admin_footer_scripts']",
-        text: "<%= render partial: 'spree/admin/shared/tinymce', locals: { ids: [ 'product_description', 'page_body', 'news_article_body' ] } %>"
+        text:          "<%= render partial: 'spree/admin/shared/tinymce', locals: { ids: [ 'product_description', 'page_body', 'news_article_body', 'news_article_summary' ] } %>",
+        original:      'bf0956ff4a1899a1707b4e30c4c4089a17dac4f4'
 )
