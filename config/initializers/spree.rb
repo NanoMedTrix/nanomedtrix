ef947abc229bd73c3f7d4ba0836755225e8c1dc1
Matthew Henry <mhenry@healthcare.uiowa.edu>
2014-02-03 18:38:28 -0600

    code formatting

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,11 +1,4 @@
-# Configure Spree Preferences
-#
-# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
-#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
-#
-# In order to initialize a setting do:
-# config.setting_name = 'new value'
-Spree.config do |config|
+Spree.config do | config |
   config.site_name = "NanoMedTrix"
   config.site_url  = "pcruagxk-nanomedtrix.herokuapp.com"
 
