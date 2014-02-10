bf928693acc8ce98b37a77d596512ea297be7924
Matthew Henry <matthew.gordon.henry@gmail.com>
2014-02-10 13:28:22 -0600

    S3 support

--- app/models/spree/document.rb
+++ app/models/spree/document.rb
@@ -13,6 +13,9 @@ module Spree
     ALLOWED_CONTENT_TYPES   = [ 'application/msword', 'application/vnd.ms-excel', 'application/pdf' ]
     ALLOWED_FILE_EXTENSIONS = [ 'pdf', 'doc', 'docx', 'xls', 'xlsx' ]
 
+    include Spree::Core::S3Support
+    supports_s3 :attachment
+
     def no_attachment_errors
       unless attachment.errors.empty?
         # uncomment this to get rid of the less-than-useful interrim messages
