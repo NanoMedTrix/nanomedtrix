0061f99d5f46ae9c019d02d29852e37965fc2041
Matthew Henry <matthew.gordon.henry@gmail.com>
2014-02-10 13:49:55 -0600

    name consistency update

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,7 +2,7 @@ Spree::Product.class_eval do
 	delegate :product_documents, to: :master, prefix: true
 
 	has_many :variant_documents, source: :product_documents, through: :variants_including_master, order: :position
-	alias_method :documents, :master_documents
+	alias_method :documents, :master_product_documents
 
   def self.find_by_array_of_ids ids
     products = Spree::Product.find( :all, conditions: [ "id IN (?)", ids ] )
