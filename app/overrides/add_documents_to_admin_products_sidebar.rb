001d28eb35df85ad912a112d89ea444b5a7c41a3
Matthew Henry <matthew.gordon.henry@gmail.com>
2014-02-10 13:21:38 -0600

    product documents link on admin products page

---
+++ app/overrides/add_documents_to_admin_products_sidebar.rb
@@ -0,0 +1,7 @@
+Deface::Override.new(
+	virtual_path:  'spree/admin/shared/_product_tabs', 
+	name:          'docs_sidebar',
+	insert_bottom: "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]", 
+	partial:       'spree/admin/products/documents',
+	disabled:      false
+)
