5ff19efdc224522541731188975df38dc7eaa341
Matthew Henry <matthew.gordon.henry@gmail.com>
2014-02-10 12:38:32 -0600

    product documents relation to product model

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,4 +1,9 @@
 Spree::Product.class_eval do
+	delegate :documents, to: :master, prefix: true
+
+	has_many :variant_documents, source: :documents, through: :variants_including_master, order: :position
+	alias_method :documents, :master_documents
+
   def self.find_by_array_of_ids ids
     products = Spree::Product.find( :all, conditions: [ "id IN (?)", ids ] )
     ids.map{ | id | products.detect{ | p | p.id == id.to_i } }.compact
