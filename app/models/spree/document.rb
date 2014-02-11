0aee1f608d8c0133e01d9f5df3fb05b74ec86de8
Matthew Henry <matthew.gordon.henry@gmail.com>
2014-02-10 18:51:50 -0600

    undo

--- app/models/spree/document.rb
+++ app/models/spree/document.rb
@@ -14,12 +14,6 @@ module Spree
     include Spree::Core::S3Support
     supports_s3 :attachment
 
-    Spree::Document.attachment_definitions[ :attachment ][ :styles ]        = ActiveSupport::JSON.decode( Spree::Config[ :attachment_styles ] ).symbolize_keys!
-    Spree::Document.attachment_definitions[ :attachment ][ :path ]          = ":rails_root/public/assets/product/documents/:id/:style/:basename.:extension"
-    Spree::Document.attachment_definitions[ :attachment ][ :url ]           = "/assets/product/documents/:id/:style/:basename.:extension"
-    Spree::Document.attachment_definitions[ :attachment ][ :default_url ]   = Spree::Config[ :attachment_default_url ]
-    Spree::Document.attachment_definitions[ :attachment ][ :default_style ] = Spree::Config[ :attachment_default_style ]
-
     def no_attachment_errors
       unless attachment.errors.empty?
         # uncomment this to get rid of the less-than-useful interrim messages
