6aa758ad3b7f2db3f22fbc0328f4f9cb9373999b
Matthew Henry <matthew.gordon.henry@gmail.com>
2014-02-19 17:40:30 -0600

    converting orders index page to bootstrap

---
+++ app/helpers/spree/admin/tables_helper_decorator.rb
@@ -0,0 +1,18 @@
+module Spree
+  module Admin
+    module TablesHelper
+      def row_state state 
+        case state 
+        when 'complete'
+          'success'
+        when 'cart'
+          'warning'
+        when 'canceled'
+          'danger'
+        else
+          nil
+        end
+      end
+    end
+  end
+end
